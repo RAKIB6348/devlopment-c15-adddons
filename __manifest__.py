dcf8f51fe6f88926294e09344a31776c99d08b7d
Rakib48-Code <shahidulhuda0@gmail.com>
2025-02-04 11:41:53 +0600

    add icon

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'odoo15 development',
+    'version' :  '15.1.1.1.2',
+    'category' : 'Tutorials',
+    'summary' : 'odoo15 developemnt tutorials by odoo mates',
+    'sequence' : 10,
+    'depends' : [],
+    'data' : [],
+    'installable' : True,
+    'application' : True,
+}
